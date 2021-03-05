1d9b76e1157fecce202b61252906b4a32c6de8b8
Kamyar Ghasemlou <kamyar.ghasemlou@deliveryhero.com>
2021-03-05 20:11:33 +0100

    CV and some basic info

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
-      const res = await fetch('/api/date');
+      const res = await fetch('/api/hello');
       const newDate = await res.text();
       setDate(newDate);
     }
@@ -14,41 +14,15 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Kamyar Ghasemlou</h1>
       <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
+        Hi there 👋, welcome to my homepage!
       </h2>
       <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
+        Intro.
       </p>
       <br />
-      <h2>The date according to Go is:</h2>
+      <h2>Backend says:</h2>
       <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
