32f251cabe4988e1e84f08e957b280b8fac5668d
Kamyar Ghasemlou <kamyar.ghasemlou@deliveryhero.com>
2021-03-05 20:38:32 +0100

    Some more testing

--- api/index.py
+++ api/index.py
@@ -1,10 +1,33 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(root_path="/api")
+
+@app.get("/", tags=["Root"])
+async def plain_api():
+  return {
+    "message": "Hello /"
+   }
 
 
 @app.get("/api/", tags=["Root"])
-async def read_root():
+async def plain_api():
   return {
-    "message": "Welcome to my notes application, use the /docs route to proceed"
+    "message": "Hello /api/"
    }
+
+
+@app.get("/{req_path:path}")
+async def serve_my_app(request: Request, req_path: str):
+    print(f"path: {req_path}")
+    return {
+      "message": "Hello /{path}"
+ 	}
+
+
+
+@app.get("/api/{req_path:path}")
+async def serve_my_app(request: Request, req_path: str):
+    print(f"path: {req_path}")
+    return {
+      "message": "Hello /api/{path}"
+ 	}
